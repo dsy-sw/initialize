d7f155fe9fccaf0b38a3c1ab842829d41d5dbafc
dsy-sw <dsy94.sw@gmail.com>
2022-05-15 01:16:24 +0900

    to add initialization file for ubuntu ver.1804

---
+++ ubuntu_1804/install_git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install git -y
+
+echo "end git installation"
+
+git config --global user.name "dsy-sw"
+git config --global user.email "dsy94.sw@gmail.com"
+
+echo "successfully enter infomation"
+
